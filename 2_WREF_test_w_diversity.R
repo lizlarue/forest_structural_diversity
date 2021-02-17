b2f81520030dd91bd0e6a1c7c8cab9b7bc872b41
nagyrc <nagyrc@gmail.com>
2021-02-17 16:46:55 -0700

    adding in spectral diversity metric

--- 2_WREF_test_w_diversity.R
+++ 2_WREF_test_w_diversity.R
@@ -140,22 +140,49 @@ WREF_table <- WREF_table %>%
   left_join(veg_types)
 
 
+#############################################
+#calculate spectral reflectance as CV
+#as defined here: https://www.mdpi.com/2072-4292/8/3/214/htm
+f <- paste0(wd,"NEON_D16_WREF_DP3_593000_5077000_reflectance.h5")
+
+
+###
+#for each of the 426 bands, I need to calculate the mean reflectance and the SD reflectance across all pixels 
+
+myNoDataValue <- as.numeric(reflInfo$Data_Ignore_Value)
+
+dat <- data.frame()
+
+for (i in 1:426){
+  #extract one band
+  b <- h5read(f,"/WREF/Reflectance/Reflectance_Data",index=list(i,1:nCols,1:nRows)) 
+  
+  # set all values equal to -9999 to NA
+  b[b == myNoDataValue] <- NA
+  
+  #calculate mean and sd
+  meanref <- mean(b)
+  SDref <- sd(b)
+  
+  rowz <- cbind(i, meanref, SDref)
+  
+  dat <- rbind(dat, rowz)
+}
+
+dat
+
+dat$calc <- dat$SDref/dat$meanref
+
+CV <- sum(dat$calc)/426
+
+
+WREF_table$specCV <- CV
+
 combo <- rbind(NIWO_table, WREF_table)
 combo
 
-#####
-#for all sites
-#all_sites_table <- all_sites_table %>%
-  #select(-easting, -northing)
 
 
-#carSpeeds <- read.csv(file = 'data/car-speeds.csv')
-#veg_types <- read.csv(file = 'field-sites.csv') %>%
-  #select(Site.ID, Dominant.NLCD.Classes)
-
-#add veg class to table 
-#all_sites_table <- all_sites_table %>%
-  #left_join(veg_types)
 
 
 
