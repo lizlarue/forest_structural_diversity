881baa03dc420ec773b0c1068fa6f4ba311b27bc
nagyrc <nagyrc@gmail.com>
2021-02-18 10:40:06 -0700

    adding spectral diversity metric

--- 12_SCBI_test_w_diversity.R
+++ 12_SCBI_test_w_diversity.R
@@ -142,6 +142,45 @@ SCBI_table <- SCBI_table %>%
   left_join(veg_types)
 
 
+
+#############################################
+#calculate spectral reflectance as CV
+#as defined here: https://www.mdpi.com/2072-4292/8/3/214/htm
+f <- paste0(wd,"NEON_D02_SCBI_DP3_750000_4308000_reflectance.h5")
+
+
+###
+#for each of the 426 bands, I need to calculate the mean reflectance and the SD reflectance across all pixels 
+
+myNoDataValue <- as.numeric(reflInfo$Data_Ignore_Value)
+
+dat <- data.frame()
+
+for (i in 1:426){
+  #extract one band
+  b <- h5read(f,"/SCBI/Reflectance/Reflectance_Data",index=list(i,1:nCols,1:nRows)) 
+  
+  # set all values equal to -9999 to NA
+  b[b == myNoDataValue] <- NA
+  
+  #calculate mean and sd
+  meanref <- mean(b, na.rm = TRUE)
+  SDref <- sd(b, na.rm = TRUE)
+  
+  rowz <- cbind(i, meanref, SDref)
+  
+  dat <- rbind(dat, rowz)
+}
+
+
+dat$calc <- dat$SDref/dat$meanref
+
+CV <- sum(dat$calc)/426
+
+
+SCBI_table$specCV <- CV
+
+
 combo11 <- rbind(combo10, SCBI_table)
 combo11
 
@@ -150,14 +189,3 @@ combo11
 
 
 ###################################
-write.table(combo11, file = "prelim_results.csv", sep = ",", row.names = FALSE)
-
-library(ggplot2)
-ggplot(combo11, aes(x = mean.max.canopy.ht.aop, y = exotic_SR))+
-  geom_point()
-
-ggplot(combo11, aes(x = max.canopy.ht.aop, y = exotic_SR))+
-  geom_point()
-
-ggplot(combo11, aes(x = rumple.aop, y = exotic_SR))+
-  geom_point()
