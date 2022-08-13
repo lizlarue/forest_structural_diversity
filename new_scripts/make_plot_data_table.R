0aaa1d65a81cbe3e4c986bdb5e38873a359d6531
Mukund Kalantri <37955246+mukundkalantri23@users.noreply.github.com>
2022-08-13 13:37:59 -0600

    Update make_plot_data_table.R

--- new_scripts/make_plot_data_table.R
+++ new_scripts/make_plot_data_table.R
@@ -2,32 +2,32 @@ library(lidR)
 library(gstat)
 library(neonUtilities)
 library(dplyr)
+source("helper.R")
 
 data <- read.csv("NEON_sites_dates_for_cover.csv", colClasses = "character")
 coverdata <- read.csv("prelim_cover.csv")
 all_neon_plot_centroids_data <- read.csv("All_NEON_TOS_Plot_Centroids_V8.csv", colClasses = "character")
 
 plot_data_table = data.frame()
-extra <- c()
 
 slicedcoverdata <- unique(coverdata %>% select(siteID, monthyear, sitemonthyear, plotID))
-for (i in 1:nrow(slicedcoverdata)){ #nrow(t1)
+for (i in 1:nrow(slicedcoverdata)){
   slice <- all_neon_plot_centroids_data[all_neon_plot_centroids_data$plotID == slicedcoverdata$plotID[i] & all_neon_plot_centroids_data$subtype == "basePlot",]
   if (nrow(slice) > 0){
     row <- cbind(slicedcoverdata[i,], slice$latitude, slice$longitude, slice$easting, slice$northing)
     plot_data_table <- rbind(plot_data_table, row)
   }
   else{
-    extra <- c(extra, as.character(slicedcoverdata[i,]$plotID))
+    slice <- coverdata[coverdata$plotID == slicedcoverdata$plotID[i],][1,]
+    slice <- slice %>% rename(latitude = "decimalLatitude", longitude = "decimalLongitude")
+    utm_coords <- longlat_to_UTM(slice$longitude, slice$latitude)
+    row <- cbind(slicedcoverdata[i,], slice$latitude, slice$longitude, "slice$easting" = utm_coords$easting, "slice$northing" = utm_coords$northing)
+    plot_data_table <- rbind(plot_data_table, row)
   }
 }
 plot_data_table <- plot_data_table %>% rename(latitude = "slice$latitude", 
                                               longitude = "slice$longitude",
                                               easting = "slice$easting",
                                               northing = "slice$northing")
-extra <- unique(extra)
-#"OSBS_051" "OSBS_018" "OSBS_023" "SCBI_033" "SCBI_041" "SCBI_044"
-#"SCBI_034" "SCBI_039" "SCBI_042" "SCBI_012" "MLBS_031" "BLAN_002"
-#"DELA_010" "DELA_007" "DELA_019" "DELA_024"
 
 write.table(plot_data_table, file = "plot_data_table.csv", sep = ",", row.names = FALSE)
